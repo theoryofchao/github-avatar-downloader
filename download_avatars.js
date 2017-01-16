5567e5fa147bb654552f4b3dfe7696816352ba64
theoryofchao <roger.ti.chao@gmail.com>
2017-01-16 15:01:09 -0500

    avatar request made step 5

--- download_avatars.js
+++ download_avatars.js
@@ -9,14 +9,16 @@ var GITHUB_TOKEN = `ee332f8ba77bb666bdf4e99ddbdba563146ce545`;
 
 
 var getRepoContributors = function(repoOwner, repoName, callback) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL)    
-        callback();  
+  var requestUrl = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestHeaders = { 
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
+    'Content-Type' : 'application/x-www-form-urlencoded' 
+  };
+  console.log(requestUrl);
+  request.get({url: requestUrl, headers: requestHeaders}, callback);
 };
 
+
 getRepoContributors(repoOwner, repoName, (err, result) => {
-  console.log(repoOwner);
-  console.log(repoName);
-  console.log(err);
-  console.log(result);
+  console.log("Results:",result);  
 });
