70bf7f44b62c366ec4d0858e1bca4c2d41e8d3a3
theoryofchao <roger.ti.chao@gmail.com>
2017-01-16 14:12:02 -0500

    download avatar credentials added

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,18 @@
 var request = require('request');
 
-console.log('Welcome to the Github Avatar Downloader!');
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
+
+var GITHUB_USER = `theoryofchao`;
+var GITHUB_TOKEN = `ee332f8ba77bb666bdf4e99ddbdba563146ce545`;
+
+var getRepoContributors = function(repoOwner, repoName, callback) {
+  callback();  
+};
+
+getRepoContributors(repoOwner, repoName, (err, result) => {
+  console.log(repoOwner);
+  console.log(repoName);
+  console.log(err);
+  console.log(result);
+});
